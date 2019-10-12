16d7e4681524c68ceb464779c2a6405cb1d1e848
João Pedro <joaopedro453@hotmail.com>
2019-10-12 20:38:53 -0300

    imagens sheets atualizadas

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var config = {
 	type: Phaser.AUTO,
-	width: 800,
+	width: 1800,
 	height: 600,
 	physics: {
 		default: 'Arcade',
 		arcade: {
 			gravity: {
-				x: 0 
+				x: -50
 			},
 			debug: false
 		}
@@ -22,21 +22,27 @@ var game = new Phaser.Game(config);
 
 function preload() {
 	this.load.image('player', 'img/player.png')
-	this.load.image('sheet', 'img/sheet.png')
-	this.load.image('bola','img/bola.png')
-
+	this.load.image('sheet1', 'img/sheetinicio.png')
+	this.load.image('sheet2', 'img/sheetpadrao.png')
+	this.load.image('bola', 'img/bola.png')
 }
 
 function create() {
-	let sheet = this.physics.add.image(1800,300,'sheet')
-	sheet.setVelocityX(-250);
-	this.add.image(400,300,'player')
+	// primeira sheet
+	this.physics.add.image(1800, 300, 'sheet1')
 
-	setTimeout(() => {
-		let quadrado = this.physics.add.image(1400,680,'bola')
-		quadrado.setVelocityX(-250);
-	}, 0);
-	
+	// sheets padrao
+	this.physics.add.image(2700, 300, 'sheet2')
+	this.physics.add.image(3500, 300, 'sheet2')
+
+	// jogador (barra preta)
+	this.add.image(400, 300, 'player')
+
+	// bolas
+	this.physics.add.image(2200, 680, 'bola')
+
+	// quadrado.setVelocityX(-250);
+
 }
 
 function update() {}
