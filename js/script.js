88315b43174eb555daf3492c5ad3b6991f5a1a1e
João Pedro <joaopedro453@hotmail.com>
2019-10-13 02:14:18 -0300

    configuração das sheets

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var config = {
 		default: 'Arcade',
 		arcade: {
 			gravity: {
-				x: -2
+				x: 0
 			},
 			debug: false
 		}
@@ -18,6 +18,7 @@ var config = {
 	}
 };
 
+var player
 var game = new Phaser.Game(config);
 
 function preload() {
@@ -25,27 +26,48 @@ function preload() {
 	this.load.image('sheet1', 'img/sheetinicio.png')
 	this.load.image('sheet2', 'img/sheetpadrao.png')
 	this.load.image('bola', 'img/bola.png')
-	this.load.image('bolinha','img/bolinha.png')
-	this.load.image('bolinha1','img/bolinhaReta.png')
-
+	this.load.image('bolinha', 'img/bolinha.png')
+	this.load.image('bolinha1', 'img/bolinhaReta.png')
 }
 
 function create() {
-	// primeira sheet
-	this.physics.add.image(1800, 300, 'sheet1')
-
-	// sheets padrao
-	this.physics.add.image(2700, 300, 'sheet2')
-	this.physics.add.image(3500, 300, 'sheet2')
+	// sheets
+	let sheets = this.physics.add.group()
+	// sheet com clavas
+	sheets.create(1800,300,`sheet1`)
+	// sheets normais
+	sheets.create(2700,300,`sheet2`)
+	sheets.create(3500,300,`sheet2`)
+	sheets.create(4300,300,`sheet2`)
+	sheets.create(5100,300,`sheet2`)
+	sheets.create(5900,300,`sheet2`)
+	sheets.create(6700,300,`sheet2`)
+	sheets.create(7500,300,`sheet2`)
+	sheets.create(8300,300,`sheet2`)
+	sheets.create(9100,300,`sheet2`)
+	sheets.create(9900,300,`sheet2`)
+	sheets.create(10700,300,`sheet2`)
+	// config
+	sheets.setVelocityX(-70);
 
 	// jogador (barra preta)
-	this.add.image(400, 300, 'player')
+	let player = this.physics.add.image(400, 300, 'player')
+	// config
+	player.setCollideWorldBounds(true);
 
-	// bolas
-	this.physics.add.image(1530, 195, 'bolinha1')
 
-	// quadrado.setVelocityX(-250);
-
+	// teclas
+	cursors = this.input.keyboard.createCursorKeys();
+	// interações
+	this.physics.add.overlap(player, dos, teste, null, this);
 }
 
-function update() {}
+function update() {
+	if (cursors.up.isDown) {}
+}
+
+function teste(player, dos) {
+	if (cursors.up.isDown) {
+		dos.disableBody(true, true);
+	}
+}
