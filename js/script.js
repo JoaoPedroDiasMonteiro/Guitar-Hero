5955aec683c7e75c1bde042aa7a020949ee5d57f
João Pedro <joaopedro453@hotmail.com>
2019-10-17 05:20:23 -0300

    sprites configurados

--- js/script.js
+++ js/script.js
@@ -38,10 +38,9 @@ function preload() {
 	this.load.image('player', 'img/player.png')
 	this.load.image('sheet1', 'img/sheetinicio.png')
 	this.load.image('sheet2', 'img/sheetpadrao.png')
-	this.load.image('bola', 'img/bola.png')
-	this.load.image('bolinha', 'img/bolinha.png')
-	this.load.image('bolinha1', 'img/bolinhaReta.png')
 	this.load.image('inimigo', 'img/inimigo.png')
+	this.load.image('bolaRisco', 'img/bolaRisco.png')
+	this.load.image('bolaRiscoBranco', 'img/bolaRiscoBranca.png')
 	this.load.audio('littlestar', 'music/littleStar.mp3')
 }
 
@@ -121,181 +120,181 @@ function create() {
 		let mi = this.physics.add.group()
 		let re = this.physics.add.group()
 
-		dó.create(1580, 268, 'bolinha1')
-		dó.create(1780, 268, 'bolinha1')
-		sol.create(1980, 195, 'bolinha1')
-		sol.create(2180, 195, 'bolinha1')
+		dó.create(1580, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(1780, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(1980, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(2180, 195, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		la.create(2380, 176, 'bolinha1')
-		la.create(2580, 176, 'bolinha1')
-		sol.create(2780, 195, 'bolinha1')
+		la.create(2380, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		la.create(2580, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(2780, 195, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		//
-		fa.create(3180, 213, 'bolinha1')
-		fa.create(3380, 213, 'bolinha1')
-		mi.create(3580, 232, 'bolinha1')
-		mi.create(3780, 232, 'bolinha1')
+		fa.create(3180, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(3380, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(3580, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(3780, 232, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		re.create(3980, 250, 'bolinha1')
-		re.create(4180, 250, 'bolinha1')
-		dó.create(4380, 268, 'bolinha1')
+		re.create(3980, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(4180, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(4380, 268, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		// segunda linha
 		//
-		sol.create(4780, 195, 'bolinha1')
-		sol.create(4980, 195, 'bolinha1')
-		fa.create(5180, 213, 'bolinha1')
-		fa.create(5380, 213, 'bolinha1')
+		sol.create(4780, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(4980, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(5180, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(5380, 213, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		mi.create(5580, 232, 'bolinha1')
-		mi.create(5780, 232, 'bolinha1')
-		re.create(5980, 250, 'bolinha1')
+		mi.create(5580, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(5780, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(5980, 250, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		//
-		sol.create(6380, 195, 'bolinha1')
-		sol.create(6580, 195, 'bolinha1')
-		fa.create(6780, 213, 'bolinha1')
-		fa.create(6980, 213, 'bolinha1')
+		sol.create(6380, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(6580, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(6780, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(6980, 213, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		mi.create(7180, 232, 'bolinha1')
-		mi.create(7380, 232, 'bolinha1')
-		re.create(7580, 250, 'bolinha1')
+		mi.create(7180, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(7380, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(7580, 250, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		// terceira linha
 		//
-		dó.create(7980, 268, 'bolinha1')
-		dó.create(8180, 268, 'bolinha1')
-		sol.create(8380, 195, 'bolinha1')
-		sol.create(8580, 195, 'bolinha1')
+		dó.create(7980, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(8180, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(8380, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(8580, 195, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		la.create(8780, 176, 'bolinha1')
-		la.create(8980, 176, 'bolinha1')
-		sol.create(9180, 195, 'bolinha1')
+		la.create(8780, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		la.create(8980, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(9180, 195, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		//
-		fa.create(9580, 213, 'bolinha1')
-		fa.create(9780, 213, 'bolinha1')
-		mi.create(9980, 232, 'bolinha1')
-		mi.create(10180, 232, 'bolinha1')
+		fa.create(9580, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(9780, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(9980, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(10180, 232, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		re.create(10380, 250, 'bolinha1')
-		re.create(10580, 250, 'bolinha1')
-		dó.create(10780, 268, 'bolinha1')
+		re.create(10380, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(10580, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(10780, 268, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 
 		//
 		// part 2 vruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuum
 		//
 
-		dó.create(12780, 268, 'bolinha1')
-		dó.create(12980, 268, 'bolinha1')
-		sol.create(13180, 195, 'bolinha1')
-		sol.create(13380, 195, 'bolinha1')
+		dó.create(12780, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(12980, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(13180, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(13380, 195, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		la.create(13580, 176, 'bolinha1')
-		la.create(13780, 176, 'bolinha1')
-		sol.create(13980, 195, 'bolinha1')
+		la.create(13580, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		la.create(13780, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(13980, 195, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		//
-		fa.create(14380, 213, 'bolinha1')
-		fa.create(14580, 213, 'bolinha1')
-		mi.create(14780, 232, 'bolinha1')
-		mi.create(14980, 232, 'bolinha1')
+		fa.create(14380, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(14580, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(14780, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(14980, 232, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		re.create(15180, 250, 'bolinha1')
-		re.create(15380, 250, 'bolinha1')
-		dó.create(15580, 268, 'bolinha1')
+		re.create(15180, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(15380, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(15580, 268, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		// segunda linha
 		//
-		sol.create(15980, 195, 'bolinha1')
-		sol.create(16180, 195, 'bolinha1')
-		fa.create(16380, 213, 'bolinha1')
-		fa.create(16580, 213, 'bolinha1')
+		sol.create(15980, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(16180, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(16380, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(16580, 213, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		mi.create(16780, 232, 'bolinha1')
-		mi.create(16980, 232, 'bolinha1')
-		re.create(17180, 250, 'bolinha1')
+		mi.create(16780, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(16980, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(17180, 250, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		//
-		sol.create(17580, 195, 'bolinha1')
-		sol.create(17780, 195, 'bolinha1')
-		fa.create(17980, 213, 'bolinha1')
-		fa.create(18180, 213, 'bolinha1')
+		sol.create(17580, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(17780, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(17980, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(18180, 213, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		mi.create(18380, 232, 'bolinha1')
-		mi.create(18580, 232, 'bolinha1')
-		re.create(18780, 250, 'bolinha1')
+		mi.create(18380, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(18580, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(18780, 250, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		// terceira linha
 		//
-		dó.create(19180, 268, 'bolinha1')
-		dó.create(19380, 268, 'bolinha1')
-		sol.create(19580, 195, 'bolinha1')
-		sol.create(19780, 195, 'bolinha1')
+		dó.create(19180, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(19380, 268, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(19580, 195, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(19780, 195, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		la.create(19980, 176, 'bolinha1')
-		la.create(20180, 176, 'bolinha1')
-		sol.create(20380, 195, 'bolinha1')
+		la.create(19980, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		la.create(20180, 176, 'bolaRisco').setOrigin(0.5, 0.86)
+		sol.create(20380, 195, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 		//
-		fa.create(20780, 213, 'bolinha1')
-		fa.create(20980, 213, 'bolinha1')
-		mi.create(21180, 232, 'bolinha1')
-		mi.create(21380, 232, 'bolinha1')
+		fa.create(20780, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		fa.create(20980, 213, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(21180, 232, 'bolaRisco').setOrigin(0.5, 0.86)
+		mi.create(21380, 232, 'bolaRisco').setOrigin(0.5, 0.86)
 		//
-		re.create(21580, 250, 'bolinha1')
-		re.create(21780, 250, 'bolinha1')
-		dó.create(21980, 268, 'bolinha1')
+		re.create(21580, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		re.create(21780, 250, 'bolaRisco').setOrigin(0.5, 0.86)
+		dó.create(21980, 268, 'bolaRiscoBranco').setOrigin(0.5, 0.86)
 
 		// 
 		// part 3 vruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuum
 		//
 		fim += 2000
-		dó.create(fim, 268, 'bolinha1'), fim += 200
-		dó.create(fim, 268, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 200
+		dó.create(fim, 268, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		dó.create(fim, 268, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
 		//
-		la.create(fim, 176, 'bolinha1'), fim += 200
-		la.create(fim, 176, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 400
+		la.create(fim, 176, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		la.create(fim, 176, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRiscoBranco').setOrigin(0.5, 0.86), fim += 400
 		//
-		fa.create(fim, 213, 'bolinha1'), fim += 200
-		fa.create(fim, 213, 'bolinha1'), fim += 200
-		mi.create(fim, 232, 'bolinha1'), fim += 200
-		mi.create(fim, 232, 'bolinha1'), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
 		//
-		re.create(fim, 250, 'bolinha1'), fim += 200
-		re.create(fim, 250, 'bolinha1'), fim += 200
-		dó.create(fim, 268, 'bolinha1'), fim += 400
+		re.create(fim, 250, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		re.create(fim, 250, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		dó.create(fim, 268, 'bolaRiscoBranco').setOrigin(0.5, 0.86), fim += 400
 		// segunda linha
 		//
-		sol.create(fim, 195, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 200
-		fa.create(fim, 213, 'bolinha1'), fim += 200
-		fa.create(fim, 213, 'bolinha1'), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
 		//
-		mi.create(fim, 232, 'bolinha1'), fim += 200
-		mi.create(fim, 232, 'bolinha1'), fim += 200
-		re.create(fim, 250, 'bolinha1'), fim += 400
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		re.create(fim, 250, 'bolaRiscoBranco').setOrigin(0.5, 0.86), fim += 400
 		//
-		sol.create(fim, 195, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 200
-		fa.create(fim, 213, 'bolinha1'), fim += 200
-		fa.create(fim, 213, 'bolinha1'), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
 		//
-		mi.create(fim, 232, 'bolinha1'), fim += 200
-		mi.create(fim, 232, 'bolinha1'), fim += 200
-		re.create(fim, 250, 'bolinha1'), fim += 400
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		re.create(fim, 250, 'bolaRiscoBranco').setOrigin(0.5, 0.86), fim += 400
 		// terceira linha
 		//
-		dó.create(fim, 268, 'bolinha1'), fim += 200
-		dó.create(fim, 268, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 200
+		dó.create(fim, 268, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		dó.create(fim, 268, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
 		//
-		la.create(fim, 176, 'bolinha1'), fim += 200
-		la.create(fim, 176, 'bolinha1'), fim += 200
-		sol.create(fim, 195, 'bolinha1'), fim += 400
+		la.create(fim, 176, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		la.create(fim, 176, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		sol.create(fim, 195, 'bolaRiscoBranco').setOrigin(0.5, 0.86), fim += 400
 		//
-		fa.create(fim, 213, 'bolinha1'), fim += 200
-		fa.create(fim, 213, 'bolinha1'), fim += 200
-		mi.create(fim, 232, 'bolinha1'), fim += 200
-		mi.create(fim, 232, 'bolinha1'), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		fa.create(fim, 213, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
+		mi.create(fim, 232, 'bolaRisco').setOrigin(0.5, 0.86), fim += 200
 		//
-		re.create(fim, 250, 'bolinha1'), fim += 300
-		re.create(fim, 250, 'bolinha1'), fim += 300
-		dó.create(fim, 268, 'bolinha1'), fim += 400
+		re.create(fim, 250, 'bolaRisco').setOrigin(0.5, 0.86), fim += 300
+		re.create(fim, 250, 'bolaRisco').setOrigin(0.5, 0.86), fim += 300
+		dó.create(fim, 268, 'bolaRiscoBranco').setOrigin(0.5, 0.86), fim += 400
 
 		//  configs
 		dó.setVelocityX(-267)
