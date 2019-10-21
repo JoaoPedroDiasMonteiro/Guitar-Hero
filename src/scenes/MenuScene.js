e1cb3835e80ce7f06d5c0990e0cf5f1021c553d8
João Pedro <joaopedro453@hotmail.com>
2019-10-21 12:08:41 -0300

    playButton config

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -12,12 +12,37 @@ export class MenuScene extends Phaser.Scene{
 
     }
     create() {
-        this.add.image(600,400, 'play')
+        let a = 2
         this.add.image(600,500, 'options')
 
         this.sound.pauseOnBlur = false;
         this.sound.play("title_music", {
             loop: true
+        })
+
+        let playButton = this.add.image(600,400, 'play')
+        playButton.setInteractive()
+        // mouse em cima
+        playButton.on('pointerover', ()=> {
+            a%2 != 0? playButton.x = 610 : playButton.x = 590 
+            a ++
+            setTimeout(() => {
+                playButton.x = 600
+            }, 150);
+        })
+        // mouse fora
+        playButton.on('pointerout', ()=> {
+            playButton.x = 600
+        })
+        // mouse click down
+        playButton.on('pointerdown', ()=> {
+            playButton.y = 405
+            console.log('rooo');
+        })
+        // mouse click up
+        playButton.on('pointerup', ()=> {
+            playButton.y = 400
+            console.log('rooo');
         })
     }
 }
