60a796ce4e207d0cecfb1c8c96a590cfecd9da17
João Pedro <joaopedro453@hotmail.com>
2019-10-24 21:47:17 -0300

    back button animation

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -3,6 +3,7 @@ import { MenuitemAnimation } from "../scenes/MenuScene"
 import { score } from "../scenes/MusicaStar"
 var scoreTxt
 var i = 0
+var z = 2
 export class GameOverScene extends Phaser.Scene {
     constructor() {
         super({
@@ -27,26 +28,31 @@ export class GameOverScene extends Phaser.Scene {
                 fill: '#FFFFFF',
             }).setOrigin(0.5, 0);
             // Score
-            scoreTxt =  this.add.text(600, 300, `Score: 0`, {
+            scoreTxt = this.add.text(600, 300, `Score: 0`, {
                 fontFamily: 'pixel',
                 fontSize: '50px',
                 fill: '#FFFFFF',
             }).setOrigin(0.5, 0);
-            // Buttons
-            let back_button = this.add.image(600, 520, 'back')
-            MenuitemAnimation(back_button, 600, 520, menuSFX, menuSFX_click)
-            back_button.on('pointerup', () => {
-                location.reload()
-            })
+
+            var anim = setInterval(() => {
+
+                if (i <= score) {
+                    scoreTxt.setText(`Score: ${i}`)
+                    i += 1 * z
+                    z++
+                } else {
+                    clearInterval(anim)
+                    scoreTxt.setText(`Score: ${score}`)
+                    // Buttons
+                    let back_button = this.add.image(600, 520, 'back')
+                    MenuitemAnimation(back_button, 600, 520, menuSFX, menuSFX_click)
+                    back_button.on('pointerup', () => {
+                        location.reload()
+                        
+                    })
+                }
+            }, 16);
+
         }, 500);
     } // < -- Create
-    update (){
-        setInterval(() => {
-            if (i <= score) {
-                scoreTxt.setText(`Score: ${i}`)
-                i+= 10
-            } 
-        }, 501);
-        
-    } // <-- update
 }
